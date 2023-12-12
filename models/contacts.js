31781ec12e7c7a39444609f2f894b074156c41d6
Alexandrbig1 <alexsmagin1@gmail.com>
2023-12-12 13:55:43 -0600

    added joi validation

--- models/contacts.js
+++ models/contacts.js
@@ -1,6 +1,18 @@
 import fs from "fs/promises";
 import path from "path";
 import { v4 as uuidv4 } from "uuid";
+import Joi from "joi";
+
+const schema = Joi.object({
+  name: Joi.string().alphanum().min(3).max(20).required(),
+
+  phone: Joi.number().integer(),
+
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ["com", "net"] },
+  }),
+}).with("name", "phone");
 
 const contactsPath = path.resolve("models", "./contacts.json");
 
@@ -40,6 +52,8 @@ const removeContact = async (contactId) => {
 
 const addContact = async ({ name, email, phone }) => {
   try {
+    await schema.validateAsync({ name, email, phone }, { abortEarly: false });
+
     const contacts = await listContacts();
     const newContact = {
       id: uuidv4(),
