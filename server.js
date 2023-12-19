969dbc90c3738fb34404db1e56947354a5c43f2b
Alexandrbig1 <alexsmagin1@gmail.com>
2023-12-18 22:37:09 -0600

    feat: added server configuration. added mongoose. connect server. get contacts from database

--- server.js
+++ server.js
@@ -7,11 +7,12 @@ const { DB_HOST, PORT = 3000 } = process.env;
 mongoose
   .connect(DB_HOST)
   .then(() => {
+    console.log("Database connection successful");
     app.listen(PORT, () => {
       console.log(`Server running. Use our API on port: ${PORT}`);
     });
   })
   .catch((error) => {
-    console.log(error.message);
+    console.error("MongoDB connection error:", error);
     process.exit(1);
   });
